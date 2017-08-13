91821f0b8a34cfcd792ffdb33808f143da37c3e4
Vivek Menon <mail@vvkmnn.xyz>
2017-08-13 15:32:36 -0400

    Nogui.

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ friend_list = []
 # username=<your-username-here> and password=<your-password> into InstaPy
 # e.g like so InstaPy(username="instagram", password="test1234")
 
-bot = InstaPy(nogui=False)
+bot = InstaPy(nogui=True)
 bot.login()
 # bot.set_upper_follower_count(limit=2500)
 bot.like_by_tags(['#alien', u'👽'], amount=500)
