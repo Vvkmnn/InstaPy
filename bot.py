e5e5e4225f097dfcf9248a360ecf77af10f8dea0
Vivek Menon <mail@vvkmnn.xyz>
2017-08-13 15:17:35 -0400

    Moving to environment variables.

--- bot.py
+++ bot.py
@@ -3,9 +3,6 @@ from instapy import InstaPy
 # Write your automation here
 # Stuck ? Look at the github page or the examples in the examples folder
 
-insta_username = 'Aliensearchparty'
-insta_password = ''
-
 dont_like = []
 ignore_words = []
 friend_list = []
@@ -14,7 +11,7 @@ friend_list = []
 # username=<your-username-here> and password=<your-password> into InstaPy
 # e.g like so InstaPy(username="instagram", password="test1234")
 
-bot = InstaPy(username=insta_username, password=insta_password, nogui=False)
+bot = InstaPy(nogui=False)
 bot.login()
 # bot.set_upper_follower_count(limit=2500)
 bot.like_by_tags(['#alien', u'👽'], amount=500)
