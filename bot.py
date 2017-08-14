879b5c2d580ee37f84d4f0d2e6040692a836d0c4
Vivek Menon <mail@vvkmnn.xyz>
2017-08-13 21:23:18 -0400

    More testing.

--- bot.py
+++ bot.py
@@ -20,9 +20,10 @@ bot.set_do_comment(True, percentage=100)
 # bot.set_upper_follower_count(limit=2500)
 
 # Decide Targets
-bot.like_by_tags(['#alien', u'👽'], amount=500)
+
 bot.like_by_users(usernames=['Chronic.alien',
                              'Poisoneddolly'], amount=100, random=True)
+bot.like_by_tags([u'#👽', '#alien'], amount=500)
 # bot.set_dont_include(friend_list)
 # bot.set_dont_like(dont_like)
 # bot.set_ignore_if_contains(ignore_words)
